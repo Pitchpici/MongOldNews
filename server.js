7638b996e877fa4e3e709ab7047db15a465f3959
Roxana Milea <roxanamilea@ymail.com>
2018-04-27 21:10:40 -0400

    corrected path2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var PORT       = process.env.PORT || 3000;
 
 // Initialize Express
 var app       = express();
-app.use(favicon(__dirname + '/fav/favicon.png'));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 // Configure middleware
 
